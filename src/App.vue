7fa0f408e51fc542e2860b00fc2db4b24d19a0a9
Dima Kulushev <dmitrijkulushev@gmail.com>
2024-01-17 16:09:19 +0300

    todolist

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <ToDo/>
+  </div>
+</template>
+<script>
+  import ToDo from './views/ToDo.vue'
+
+  export default{
+    components:{
+      ToDo,
+    }
+  }
+
+</script>
+
+<style >
+  @import 'assets/index.css';
+</style>
